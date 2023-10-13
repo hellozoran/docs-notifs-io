e447c0c17f8f1794bca4a0d8ea01f6c6e633a3da
zoran <hello@zoranmitrovic.com>
2023-10-12 18:45:04 -0700

    Cleaned up syntax on svg component

--- theme.config.tsx
+++ theme.config.tsx
@@ -5,12 +5,12 @@ import Logo from './components/logo'
 const config: DocsThemeConfig = {
   logo: <Logo />,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/hellozoran/docs-notifs-io',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  docsRepositoryBase: 'https://github.com/hellozoran/docs-notifs-io',
   footer: {
     text: 'Notifs.io Documentation',
   },
